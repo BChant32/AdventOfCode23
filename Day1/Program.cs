b04bb676e6afbbe16f03ca440f97dd7762222e42
User <User@DESKTOP-R0RF807>
2023-12-01 19:16:21 +0000

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("day1_input.txt");
+        long sum = 0;
+        foreach (string line in lines)
+        {
+            sum += readLine(line);
+        }
+        Console.WriteLine(sum);
+    }
+
+    private static int readLine(string line)
+    {
+        line = line.Trim();
+        Regex r = new Regex(@"\d");
+        if (!r.IsMatch(line)) throw new Exception("Invalid line " + line);
+        MatchCollection mc = r.Matches(line);
+        return Int32.Parse(mc.First().Value + mc.Last().Value);
+    }
+}
