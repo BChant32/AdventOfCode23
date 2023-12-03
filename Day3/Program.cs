012a68f6fd16291c878743cfa892c044cd4e4ab0
Ben Chant <bchant32@gmail.com>
2023-12-03 12:54:18 +0000

    Day 3, 2nd half also correct

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -26,7 +26,9 @@ internal class Program
                 {
                     if (digits[i, j] == -2)
                     {
-                        sum += AddNeighbours(i, j);
+                        List<int> neighbours = FindNeighbours(i, j);
+                        if (neighbours.Count == 2)
+                            sum += neighbours[0] * neighbours[1];
                     }
                 }
             }
@@ -43,22 +45,24 @@ internal class Program
             (1, 0),
             (1, -1),
         };
-        public int AddNeighbours(int i, int j)
+        public List<int> FindNeighbours(int i, int j)
         {
-            int sum = 0;
+            List<int> neighbours = new List<int>();
             foreach ((int iOffset, int jOffset) in neighbourPositions)
             {
                 int iPos = i + iOffset;
                 int jPos = j + jOffset;
-                if (iPos >= 0 && jPos >= 0 && iPos < digits.GetLength(0) && jPos < digits.GetLength(1))
-                    sum += ConsumeNumberAt(iPos, jPos);
+                if (iPos >= 0
+                    && jPos >= 0
+                    && iPos < digits.GetLength(0)
+                    && jPos < digits.GetLength(1)
+                    && digits[iPos, jPos] >= 0)
+                    neighbours.Add(ConsumeNumberAt(iPos, jPos));
             }
-            return sum;
+            return neighbours;
         }
         public int ConsumeNumberAt(int i, int j)
         {
-            if (digits[i, j] < 0) return 0;
-
             int number = 0;
             int power = 0;
             while (j + 1 < digits.GetLength(1) && digits[i, j + 1] >= 0)
@@ -87,6 +91,8 @@ internal class Program
                         sb.Append(digits[i, j]);
                     else if (digits[i, j] == -1)
                         sb.Append('_');
+                    else if (digits[i, j] == -2)
+                        sb.Append('*');
                     else
                         sb.Append('X');
                 }
@@ -107,8 +113,10 @@ internal class Program
                         grid.digits[i, j] = digit;
                     else if (c == ".")
                         grid.digits[i, j] = -1;
-                    else
+                    else if (c == "*")
                         grid.digits[i, j] = -2;
+                    else
+                        grid.digits[i, j] = -3;
                 }
             }
             return grid;
