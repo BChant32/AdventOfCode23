d70ad3e91ffb62f50b5b86d04692c4643577f384
Ben Chant <bchant32@gmail.com>
2023-12-14 05:11:43 +0000

    Day 14, 1st half correct

---
+++ Day14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string text = File.ReadAllText("day14_input.txt");
+        text = Transpose(text.Trim());
+        long sum = 0;
+        foreach (string line in text.Trim().Split("\r\n"))
+        {
+            int slider = line.Length;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == 'O')
+                {
+                    sum += slider;
+                    Console.WriteLine(slider);
+                    slider--;
+                }
+                else if (line[i] == '#')
+                    slider = line.Length - i - 1;
+            }
+        }
+        Console.WriteLine("SUM " + sum);
+    }
+
+    private static void Calculate()
+    {
+
+    }
+
+    private static string Transpose(string text)
+    {
+        string[] lines = text.Split("\r\n");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < lines[0].Length; i++)
+        {
+            for (int j = 0; j < lines.Length; j++)
+            {
+                sb.Append(lines[j][i]);
+            }
+            sb.AppendLine();
+        }
+        return sb.ToString().Trim();
+    }
+}
