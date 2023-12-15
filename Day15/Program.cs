08a07609d94cefa87d0289b0a4b94c46bb2be6e3
Ben Chant <bchant32@gmail.com>
2023-12-15 11:33:44 +0000

    Day 15, 2nd half also correct

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -2,14 +2,55 @@
 {
     private static void Main(string[] args)
     {
+        Dictionary<int, (LinkedList<string>, Dictionary<string, int>)> boxes = new Dictionary<int, (LinkedList<string>, Dictionary<string, int>)>();
         string input = File.ReadAllText("day15_input.txt");
-        long sum = 0;
         foreach (string line in input.Trim().Split(','))
         {
-            int hash = GetHash(line);
-            Console.WriteLine(hash);
-            sum += hash;
+            string label;
+            int hash;
+            if (line.Contains('-'))
+            {
+                label = line.TrimEnd('-');
+                hash = GetHash(label);
+                if (!boxes.ContainsKey(hash)) continue;
+                (LinkedList<string> ll, Dictionary<string, int> d) = boxes[hash];
+                ll.Remove(label);
+                d.Remove(label);
+            }
+            else
+            {
+                label = line.Split('=')[0];
+                int power = Int32.Parse(line.Split('=')[1]);
+                hash = GetHash(label);
+                if (!boxes.ContainsKey(hash))
+                    boxes.Add(hash, (new LinkedList<string>([label]), new Dictionary<string, int>() { { label, power} }));
+                else
+                {
+                    (LinkedList<string> ll, Dictionary<string, int> d) = boxes[hash];
+                    if (ll.Find(label) is not null)
+                        d[label] = power;
+                    else
+                    {
+                        ll.AddLast(label);
+                        d.Add(label, power);
+                    }
+                }
+            }
         }
+
+        long sum = 0;
+        foreach (var kvp in boxes)
+        {
+            string printStr = String.Join("] [", kvp.Value.Item1.Select(s => s + ' ' + kvp.Value.Item2[s]));
+            Console.WriteLine($"Box {kvp.Key}: [{printStr}]");
+            int boxNum = kvp.Key + 1;
+            int position = 1;
+            foreach (string label in kvp.Value.Item1)
+            {
+                sum += boxNum * position++ * kvp.Value.Item2[label];
+            }
+        }
+
         Console.WriteLine("SUM " + sum);
     }
 
