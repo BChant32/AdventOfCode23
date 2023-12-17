79fd079cff7fb1dd346a6aa5ee7d64a6c04de99b
Ben Chant <bchant32@gmail.com>
2023-12-17 00:06:55 +0000

    Day 16, 2nd half also correct

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -14,18 +14,57 @@ internal class Program
         string[] lines = File.ReadAllLines("day16_input.txt");
         int height = lines.Length, width = lines[0].Length;
         char[,] contrap = new char[height, width];
-        BitArray[,] energised = new BitArray[height, width];
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
                 contrap[i,j] = lines[i][j];
-                energised[i, j] = new BitArray(4);
             }
         }
-        energised[0, 0][3] = true;
-        processLaser(ref contrap, ref energised, height, width, Direction.Down, 0, 0);
-        Console.WriteLine(energised.Cast<BitArray>().Count(ba => ba.Cast<bool>().Any(b => b)));
+
+        int highscore = 0;
+        for (int i = 0; i < height; i++)
+        {
+            BitArray[,] energised = energisedInit(height, width);
+            processLaser(ref contrap, ref energised, height, width, Direction.Right, i, -1);
+            int score = energised.Cast<BitArray>().Count(ba => ba.Cast<bool>().Any(b => b));
+            if (score > highscore) highscore = score;
+        }
+        for (int i = 0; i < height; i++)
+        {
+            BitArray[,] energised = energisedInit(height, width);
+            processLaser(ref contrap, ref energised, height, width, Direction.Left, i, width);
+            int score = energised.Cast<BitArray>().Count(ba => ba.Cast<bool>().Any(b => b));
+            if (score > highscore) highscore = score;
+        }
+        for (int j = 0; j < width; j++)
+        {
+            BitArray[,] energised = energisedInit(height, width);
+            processLaser(ref contrap, ref energised, height, width, Direction.Down, -1, j);
+            int score = energised.Cast<BitArray>().Count(ba => ba.Cast<bool>().Any(b => b));
+            if (score > highscore) highscore = score;
+        }
+        for (int j = 0; j < width; j++)
+        {
+            BitArray[,] energised = energisedInit(height, width);
+            processLaser(ref contrap, ref energised, height, width, Direction.Up, height, j);
+            int score = energised.Cast<BitArray>().Count(ba => ba.Cast<bool>().Any(b => b));
+            if (score > highscore) highscore = score;
+        }
+        Console.WriteLine(highscore);
+    }
+
+    private static BitArray[,] energisedInit(int height, int width)
+    {
+        BitArray[,] energisedBlank = new BitArray[height, width];
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                energisedBlank[i, j] = new BitArray(4);
+            }
+        }
+        return energisedBlank;
     }
 
     private static void drawContrap(char[,] contrap, BitArray[,] energised, int height, int width)
